f21e12f5dfa6db0415ce68f3240e55bc0580c411
Alexa Lewis <alexalewisdevelops@gmail.com>
2020-02-28 10:04:32 -0500

    Able to produce an album and add songs

--- Program.cs
+++ Program.cs
@@ -98,15 +98,65 @@ namespace Rhythym
           Console.WriteLine($"You have unsigned {band}.");
           db.SaveChanges();
 
-
         }
         else if (input == "produce")
         {
-          // produce a band
+          Console.WriteLine("Do you want to (CREATE) a new album or (ADD) a song?");
+          var produceInput = Console.ReadLine().ToLower();
+          if (produceInput == "create")
+          {
+            var newAlbum = new Album();
+
+            Console.WriteLine("Which band are you producing an album for? Choose by Id");
+            newAlbum.BandId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is the name of the album you want to produce?");
+            newAlbum.Title = Console.ReadLine().ToLower();
+
+            Console.WriteLine("Is the album explicit? (TRUE) for yes, (FALSE) for no");
+            newAlbum.IsExplicit = bool.Parse(Console.ReadLine().ToLower());
+
+            newAlbum.ReleaseDate = DateTime.Now;
+
+            db.Albums.Add(newAlbum);
+            db.SaveChanges();
+
+          }
+
+          if (produceInput == "add")
+          {
+            var newSong = new Song();
+
+            Console.WriteLine("Which album are you adding a song to? Choose by Id");
+            newSong.AlbumId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What song do you want to add?");
+            newSong.Title = Console.ReadLine().ToLower();
+
+            Console.WriteLine("What is the length of the song?");
+            newSong.Length = Console.ReadLine();
+
+            Console.WriteLine("What is the genre of the song?");
+            newSong.Genre = Console.ReadLine().ToLower();
+
+            Console.WriteLine("What are the lyrics to the song?");
+            newSong.Lyrics = Console.ReadLine().ToLower();
+
+            db.Songs.Add(newSong);
+            db.SaveChanges();
+          }
+
         }
         else if (input == "resign")
         {
-          // resign a band
+          Console.WriteLine("Which band do you want to resign?");
+          var band = Console.ReadLine().ToLower();
+
+          var bandToResign = db.Bands.FirstOrDefault(b => b.Name == band);
+          bandToResign.IsSigned = true;
+
+          Console.WriteLine($"You have resigned {band}.");
+          db.SaveChanges();
         }
         else if (input == "view")
         {
